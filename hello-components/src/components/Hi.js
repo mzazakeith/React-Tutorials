6f99d48d683d3ac0934eb072e5b17c7c39408b52
mzazakeith <mzazakeith@gmail.com>
2022-01-13 16:39:07 +0300

    correction made from class to className

--- hello-components/src/components/Hi.js
+++ hello-components/src/components/Hi.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Hi = () =>{
-    return React.createElement('div',{id:'hello', class:'dummyClass'}, 
+    return React.createElement('div',{id:'hello', className:'dummyClass'}, 
     React.createElement('h1',null, 'Hi Mzaza no JSX'))
 }
 
