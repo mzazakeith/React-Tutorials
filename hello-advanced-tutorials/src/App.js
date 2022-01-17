903c1eb190d3e659e09850be3c283375b9ba6e14
Keith Mzaza <mzaza@Keiths-MacBook-Pro.local>
2022-01-17 14:33:35 +0300

    React Fragments

---
+++ hello-advanced-tutorials/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FragmentDemo from './components/FragmentDemo';
+import Table from './components/Table';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <FragmentDemo/> */}
+      <Table/> 
+    </div>
+  );
+}
+
+export default App;
