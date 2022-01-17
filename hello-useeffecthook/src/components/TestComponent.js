9c88c44fec2a3886061e66e5c4c41951f8599b0b
Keith Mzaza <mzaza@Keiths-MacBook-Pro.local>
2022-01-17 16:58:46 +0300

    fetching data using effect hooks and using an  empty dependency array so it is once

--- hello-useeffecthook/src/components/TestComponent.js
+++ hello-useeffecthook/src/components/TestComponent.js
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
 function TestComponent() {
     const [count, setCount] = useState(0);
     const [darkMode, setDarkMode] = useState(false);
-
+    const [posts, setPost]= useState([]);
     // useEffect(()=> {
     //     document.title= `${count}`
     //      console.log("Use effect hook ran")
@@ -14,13 +15,24 @@ function TestComponent() {
     //      console.log(" dark mode hook ran")
     //  },[darkMode])
 
+    // useEffect(()=> {
+    //     const myTimer = setTimeout (()=>{
+    //         alert("hello")
+    //         console.log(" alert Use effect hook ran")
+    //     },5000);
+    //     return () => clearTimeout(myTimer);
+    //  },[count]);
+
     useEffect(()=> {
-        const myTimer = setTimeout (()=>{
-            alert("hello")
-            console.log(" alert Use effect hook ran")
-        },5000);
-        return () => clearTimeout(myTimer);
-     },[count]);
+        const fetchData = async () => {
+            const results = await axios.get("https://jsonplaceholder.typicode.com/posts");
+            setPost(results.data);
+            console.log(results.data)
+        };
+        fetchData()
+     },[]);
+
+
 
     return (
         <div>
