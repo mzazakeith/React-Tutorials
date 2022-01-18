bc7bf6cb162476fa71b493c502ea8e029db8677a
Keith Mzaza <mzaza@Keiths-MacBook-Pro.local>
2022-01-18 15:39:45 +0300

    dispatching actions

---
+++ redux-starter/src/index.js
@@ -0,0 +1,24 @@
+import store from './store'
+
+store.dispatch({
+    type: "bugAdded",
+    payload: {
+        description : "Bug1"
+    }
+});
+
+store.dispatch({
+    type: "bugAdded",
+    payload: {
+        description : "Bug2"
+    }
+});
+
+store.dispatch({
+    type: "bugRemoved",
+    payload:{
+        id:1
+    }
+});
+
+console.log(store.getState())
