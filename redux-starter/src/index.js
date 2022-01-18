811f896fa10489b7b5539ccc856e13b549e81342
Keith Mzaza <mzaza@Keiths-MacBook-Pro.local>
2022-01-18 13:05:58 +0300

    updating arrays - adding, removing, adding at specific, updating specific

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -32,3 +32,30 @@ const updated = {
 }
 
 console.log(updated)
+
+// updating arrays
+
+const numbers = [1,2,3]
+
+const endAdd = [...numbers, 4]
+console.log(endAdd)
+
+const frontAdd = [4, ...numbers]
+console.log(frontAdd)
+
+// adding at a specific point 
+
+const index = numbers.indexOf(2);
+const added = [...numbers.slice(0,index), 4, ...numbers.slice(index)]
+console.log(added)
+
+// removing 
+
+const removed =  numbers.filter(n => n !== 2);
+console.log(removed)
+
+
+// updating
+
+const updating = numbers.map(n => n===2 ? 20 : n)
+console.log(updating)
