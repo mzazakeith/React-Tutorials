2c5b3aebc11f1b2aa90a84f520d29a8442d7340c
Keith Mzaza <mzaza@Keiths-MacBook-Pro.local>
2022-01-18 14:01:59 +0300

    immer installed

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -63,3 +63,14 @@ console.log(updating)
 
 
 // immutability
+
+let book = Map({title:"Harry potter"});
+
+function publish(book){
+    return book.set("isPublished",true)
+}
+
+book = publish(book);
+
+// console.log(book.get("title"))
+console.log(book.toJS())
