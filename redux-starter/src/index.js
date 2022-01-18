2898d270d181697bee432075e03bb4fff9372875
Keith Mzaza <mzaza@Keiths-MacBook-Pro.local>
2022-01-18 11:59:49 +0300

    how to use compose and pipe from lodash

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+import {compose , pipe} from 'lodash/fp'; 
+
+let input = "  Javascript   "
+
+const trim= str => str.trim();
+const wrapInDiv = str => `<div> ${str}</div>`;
+const toLowerCase = str => str.toLowerCase();
+
+// compose(wrapInDiv, toLowerCase, trim)
+
+const transform= pipe(trim, toLowerCase, wrapInDiv);
+transform(input);
