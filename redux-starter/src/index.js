fe290a6aaa044a063bdac0df3a2919cae606e59f
Keith Mzaza <mzaza@Keiths-MacBook-Pro.local>
2022-01-18 16:07:25 +0300

    how to make dispacthes cleaner using action creators

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,30 +1,14 @@
 import store from './store'
-import * as actions from './actionTypes'
+import {bugAdded, bugRemoved} from './actions'
+
 
 store.subscribe(()=>{
     console.log("state changed", store.getState())
 })
 
+store.dispatch(bugAdded("New Bug"))
+store.dispatch(bugAdded("New Bug 2"))
+store.dispatch(bugRemoved(1))
 
-store.dispatch({
-    type: actions.BUG_ADDED,
-    payload: {
-        description : "Bug1"
-    }
-});
-
-store.dispatch({
-    type: actions.BUG_ADDED,
-    payload: {
-        description : "Bug2"
-    }
-});
-
-store.dispatch({
-    type: actions.BUG_REMOVED,
-    payload:{
-        id:1
-    }
-});
 
 console.log(store.getState())
