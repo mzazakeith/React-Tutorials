2816ba94c1a8408f294d2247a9de74e84dcb1af7
Keith Mzaza <mzaza@Keiths-MacBook-Pro.local>
2022-01-18 12:20:43 +0300

    using piping and currying together

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -4,10 +4,10 @@ let input = "  Javascript   "
 
 const trim= str => str.trim();
 // const wrapInDiv = str => `<div> ${str}</div>`;
-const wrap = (type,str) => `<${type}> ${str}</${type}>`;
+const wrap = type => str => `<${type}>${str}</${type}>`;
 const toLowerCase = str => str.toLowerCase();
 
 // compose(wrapInDiv, toLowerCase, trim)
 
-const transform= pipe(trim, toLowerCase, wrap);
+const transform= pipe(trim, toLowerCase, wrap("span"));
 console.log(transform(input));
