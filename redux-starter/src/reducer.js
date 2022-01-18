bd9f8be57a05c71b300fe9695f02d211b421f3b1
Keith Mzaza <mzaza@Keiths-MacBook-Pro.local>
2022-01-18 15:52:30 +0300

    changed reducer cases to use action types

--- redux-starter/src/reducer.js
+++ redux-starter/src/reducer.js
@@ -1,8 +1,10 @@
+import * as actions from './actionTypes';
+
 let lastId = 0;
 
 function reducer(state=[],action){
     switch (action.type){
-        case "bugAdded":
+        case actions.BUG_ADDED:
             return [
                 ...state,{
                     id:++lastId,
@@ -10,7 +12,7 @@ function reducer(state=[],action){
                     resolved:false
                 }
             ];
-        case "bugRemoved":
+        case actions.BUG_REMOVED:
             return state.filter(bug => bug.id !== action.payload.id);
         default:
             return state;
