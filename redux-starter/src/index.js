904bbad15b24461548f354bcbfa9a525c0058df9
Keith Mzaza <mzaza@Keiths-MacBook-Pro.local>
2022-01-18 15:54:54 +0300

    changes the dispact actions to use action types

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,4 +1,5 @@
 import store from './store'
+import * as actions from './actionTypes'
 
 store.subscribe(()=>{
     console.log("state changed", store.getState())
@@ -6,21 +7,21 @@ store.subscribe(()=>{
 
 
 store.dispatch({
-    type: "bugAdded",
+    type: actions.BUG_ADDED,
     payload: {
         description : "Bug1"
     }
 });
 
 store.dispatch({
-    type: "bugAdded",
+    type: actions.BUG_ADDED,
     payload: {
         description : "Bug2"
     }
 });
 
 store.dispatch({
-    type: "bugRemoved",
+    type: actions.BUG_REMOVED,
     payload:{
         id:1
     }
