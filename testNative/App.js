2e359f28731bed09234c242cb03858d8cb069c9e
mzazakeith <mzazakeith@gmail.com>
2022-03-16 15:39:35 +0300

    simple home page with a background image and custom component

--- testNative/App.js
+++ testNative/App.js
@@ -1,20 +1,18 @@
-
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-
+import {StyleSheet, Text, View,Platform, StatusBar} from 'react-native';
+import CarItem from "./components/CarItem/";
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </SafeAreaView>
-  );
+    return (
+        <View style={styles.container}>
+            <CarItem/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    },
 });
